4497f224db7824ee0a7bd12d158e8aeccbb2128a
Thalys <thalysfarias11@gmail.com>
2025-04-02 22:32:29 -0300

    feat: criando ingestorHandler

--- internal/handlers/ingestor_handler.go
+++ internal/handlers/ingestor_handler.go
@@ -1,5 +1,26 @@
 package handlers
 
-func Ingestor(){
-	// Lógica do ingestor...
+import (
+	"fmt"
+
+	"github.com/ThalysSilva/ingestor-consumo/internal/entities"
+	"github.com/ThalysSilva/ingestor-consumo/internal/services/pulse"
+	"github.com/gin-gonic/gin"
+)
+
+func Ingestor(pulseService pulse.PulseService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var pulso entities.Pulse
+		if err := c.ShouldBindJSON(&pulso); err != nil {
+			c.Error(err)
+			c.JSON(400, gin.H{"error": "Invalid request"})
+			return
+		}
+
+		pulseService.EnqueuePulse(pulso)
+		fmt.Printf("Recebido pulso: %v\n", pulso)
+		// no content return
+		c.JSON(201, nil)
+	}
+
 }
