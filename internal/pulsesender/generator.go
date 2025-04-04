2147d64325915f309d9a60c2cebc06b39c8552fd
Thalys <thalysfarias11@gmail.com>
2025-04-03 23:44:09 -0300

    feat: diminuindo a geração de sku para agregar melhor.

--- internal/pulsesender/generator.go
+++ internal/pulsesender/generator.go
@@ -7,7 +7,7 @@ import (
 )
 
 func GenerateRandomPulse(tenantId string, useUnit pulse.PulseUnit) (*pulse.Pulse, error) {
-	productSku := fmt.Sprintf("SKU-%d", rand.Intn(1000))
+	productSku := fmt.Sprintf("SKU-%d", rand.Intn(100))
 	usedAmount := float64(rand.Intn(1000)) + rand.Float64()
 	pulse, err := pulse.NewPulse(tenantId, productSku, usedAmount, useUnit)
 	if err != nil {
